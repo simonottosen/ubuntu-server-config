5df09d60c3cec21c33646f406e4382bc4b8b5db4
Simon Ottosen <simonkottosen@gmail.com>
2022-09-01 14:30:14 +0200

    Update frontend

--- cph/frontend/project/app.py
+++ cph/frontend/project/app.py
@@ -12,7 +12,8 @@ import numpy as np
 import streamlit as st
 import urllib.request
 import altair as alt
-from bokeh.plotting import figure
+import datetime
+import calendar
 
 @st.experimental_memo
 def load_data():
@@ -23,7 +24,8 @@ def load_data():
     StartTime = pd.to_datetime(StartTime)
     StartTime = StartTime.apply(lambda t: t.replace(tzinfo=None))
     StartTime = StartTime + pd.DateOffset(hours=2)
-    dataframe["deliveryid"] = StartTime
+    dataframe["Date and time"] = StartTime
+    dataframe["Queue"] = dataframe["t2waitingtime"]
     dataframe["Time"] = StartTime.dt.time
     dataframe["Date"] = StartTime.dt.date
     return dataframe
@@ -32,22 +34,36 @@ def load_latest():
     data = urllib.request.urlopen("https://cphapi.simonottosen.dk/waitingtime?select=id,t2waitingtime,deliveryid&order=id.desc&limit=2").read()
     output = json.loads(data)
     dataframe = pd.DataFrame(output)
-    StartTime = dataframe["deliveryid"]
-    StartTime = pd.to_datetime(StartTime)
-    StartTime = StartTime.apply(lambda t: t.replace(tzinfo=None))
-    StartTime = StartTime + pd.DateOffset(hours=2)
-    dataframe["deliveryid"] = StartTime
-    dataframe["Time"] = StartTime.dt.time
-    dataframe["Date"] = StartTime.dt.date
     delta = dataframe["t2waitingtime"][0] - dataframe["t2waitingtime"][1]
     latest = dataframe["t2waitingtime"][0]
     delta = np.int16(delta).item()
     latest = np.int16(latest).item()
     M = ' Minutes'
     latest = (str(latest) + M)
+    delta = (str(delta) + M)
     return latest, delta
 
-    
+
+def load_last_two_hours():
+    data = urllib.request.urlopen("https://cphapi.simonottosen.dk/waitingtime?select=t2waitingtime&order=id.desc&limit=24").read()
+    output = json.loads(data)
+    dataframe = pd.DataFrame(output)
+    two_hours_avg = dataframe['t2waitingtime'].to_list()
+    def Average(l): 
+        avg = sum(l) / len(l) 
+        return avg
+    average = round(Average(two_hours_avg))
+    M = ' Minutes'
+    average = (str(average) + M)
+    return average    
+
+
+def findDay(date):
+    date = str(date)
+    born = datetime.datetime.strptime(date, '%Y-%m-%d').weekday()
+    return (calendar.day_name[born])
+
+
 
 st.set_page_config(page_icon="✈️", page_title="CPH Security Queue")
 
@@ -73,21 +89,26 @@ footer:after {
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
+# Using object notation
+
+st.sidebar.header("Get queing time for your upcoming flight")
+
+date = st.sidebar.date_input(
+     "On what date is your flight?")
+
+time = st.sidebar.time_input('At what time would you expect to arrive at the airport?', help="Usually you should arrive approx. 2 hours before your flight if bringing luggage and 1 hour before if you are only bringing carry-on")
+datetime_queue_input = ('You will be flying out at ' + str(time.strftime("%H:%M")) + ' on a ' + str(findDay(date)))
+
+st.sidebar.subheader(datetime_queue_input)
+st.sidebar.button('Calculate expected security queue')
 
 
-df = load_data()
+dataframe = load_data()
 currenttime = load_latest()
-st.title("CPH Security Queue")
-st.metric(label="Current waiting time", value=currenttime[0], delta=currenttime[1])
+average = load_last_two_hours()
+st.title("✈️ CPH Security Queue")
+col1, col2 = st.columns(2)
+col1.metric(label="Current waiting time", value=currenttime[0], delta=currenttime[1], delta_color="inverse")
+col2.metric(label="Average waiting time in last 2 hours", value=average)
 
-p = figure(
-     title='Overview of CPH queuing time',
-     x_axis_type='datetime',
-     x_axis_label='Time',
-     y_axis_label='Queue')
-
-p.line(df["deliveryid"], df["t2waitingtime"], legend_label='Trend', line_width=2)
-
-st.bokeh_chart(p, use_container_width=True)
-st.dataframe(df)
-
+st.line_chart(dataframe, x="Date and time", y="Queue", )
