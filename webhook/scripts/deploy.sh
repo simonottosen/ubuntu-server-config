2017f7c2a72fd27a27a5099af13b911b323d11ae
simonottosen <simonkottosen@gmail.com>
2023-01-08 18:16:16 +0100

    Update deploy.sh

--- webhook/scripts/deploy.sh
+++ webhook/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "cd /home/simonottosen/mediaserver" > /hostpipe/deploy
-echo "docker compose pull" > /hostpipe/deploy
-echo "git pull https://github.com/simonottosen/ubuntu-server-config.git" > /hostpipe/deploy
-echo "docker compose up -d" > /hostpipe/deploy
+echo "cd /home/simonottosen/mediaserver" > /pipe/deploy
+echo "docker compose pull" > /pipe/deploy
+echo "git pull https://github.com/simonottosen/ubuntu-server-config.git" > /pipe/deploy
+echo "docker compose up -d --remove-orphans" > /pipe/deploy
